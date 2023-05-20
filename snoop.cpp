7444bc0c5c2c22f538f307e17c87ac7c03d486b6
Mandrew0822 <110215582+Mandrew0822@users.noreply.github.com>
2023-05-20 14:48:47 -0400

    Add executable detection feature

--- snoop.cpp
+++ snoop.cpp
@@ -11,6 +11,8 @@
 // Function to get the file type based on the file extension
 std::string getFileType(const std::string& filename) {
     static const std::map<std::string, std::string> extensionMap = {
+    std::string getFileType(const std::string& filename) {
+    static const std::map<std::string, std::string> extensionMap = {
         { ".cpp", "C++ Source Code" },
         { ".cc", "C++ Source Code" },
         { ".cxx", "C++ Source Code" },
@@ -37,39 +39,38 @@ std::string getFileType(const std::string& filename) {
         { ".asm", "Assembly Source Code" },
         { ".S", "Assembly Source Code" },
         { ".txt", "Text File" },
-        { ".pdf", "Image File" },
-        { ".jpg", "Image File" },
-        { ".jpeg", "Image File" },
+        { ".pdf", "PDF Document" },
         { ".doc", "Microsoft Word Document" },
         { ".docx", "Microsoft Word Document" },
         { ".xls", "Microsoft Excel Spreadsheet" },
         { ".xlsx", "Microsoft Excel Spreadsheet" },
-        { ".ppt", "Microsoft Powerpoint Presentation" },
-        { ".pptx", "Microsoft Powerpoint Presentation" },
+        { ".ppt", "Microsoft PowerPoint Presentation" },
+        { ".pptx", "Microsoft PowerPoint Presentation" },
         { ".gif", "Graphics Interchange Format" },
-        { ".mp3", "Audio file" },
-        { ".mp4", "Video File" },
-        { ".mkv", "Video File" },
-        { ".zip", "Zip Archive File" },
-        { ".tar", "Tape Archive File" },
+        { ".png", "Portable Network Graphics" },
+        { ".jpg", "JPEG Image" },
+        { ".jpeg", "JPEG Image" },
+        { ".bmp", "Bitmap Image" },
+        { ".svg", "Scalable Vector Graphics" },
+        { ".mp3", "MP3 Audio" },
+        { ".mp4", "MP4 Video" },
+        { ".mkv", "Matroska Video" },
+        { ".zip", "Zip Archive" },
+        { ".tar", "Tape Archive" },
         { ".exe", "Windows Executable File" },
-        { ".deb", "Debian/Ubuntu Package file" },
+        { ".deb", "Debian/Ubuntu Package File" },
         { ".rpm", "Red Hat/Fedora Package File" },
-        { ".iso", "CD/DVD Disk Image File" },
-        { ".so", "Linux Shared Object Library File" },
-        { ".odt", "OpenDocument text" },
+        { ".iso", "CD/DVD Disk Image" },
+        { ".so", "Shared Object Library" },
+        { ".odt", "OpenDocument Text" },
         { ".rtf", "Rich Text Format" },
-        { ".html", "Hypertext Markup Language" },
-        { ".xml", "eXtensible Markup Language" },
+        { ".html", "HTML Document" },
+        { ".xml", "XML Document" },
         { ".ods", "OpenDocument Spreadsheet" },
         { ".csv", "Comma-Separated Values" },
         { ".tsv", "Tab-Separated Values" },
         { ".odp", "OpenDocument Presentation" },
-        { ".bmp", "Bitmap Image" },
-        { ".tiff", "Tagged Image File Format" },
-        { ".svg", "Scalable Vector Graphics" },
-        { ".psd", "Adobe Photoshop Document" },
-        { ".wav", "Waveform Audio File Format" },
+        { ".wav", "Waveform Audio File" },
         { ".aac", "Advanced Audio Coding" },
         { ".flac", "Free Lossless Audio Codec" },
         { ".ogg", "Ogg Vorbis" },
@@ -78,17 +79,17 @@ std::string getFileType(const std::string& filename) {
         { ".mov", "QuickTime Movie" },
         { ".wmv", "Windows Media Video" },
         { ".flv", "Flash Video" },
-        { ".rar", "Roshal Archive" },
-        { ".7z", "7-zip Compressed File" },
+        { ".rar", "RAR Archive" },
+        { ".7z", "7-Zip Compressed File" },
         { ".bz2", "Bzip2 Compressed File" },
         { ".sql", "Structured Query Language" },
         { ".db", "Database File" },
         { ".accdb", "Microsoft Access Database" },
-        { ".json", "Javascript Object Notation" },
+        { ".json", "JavaScript Object Notation" },
         { ".dll", "Dynamic Link Library" },
         { ".apk", "Android Package File" },
         { ".ttf", "TrueType Font File" },
-        { ".otf", "OpenType Font file" },
+        { ".otf", "OpenType Font File" },
     };
 
     std::size_t dotIndex = filename.find_last_of(".");
@@ -125,6 +126,11 @@ std::string getPermissionString(mode_t mode) {
     return permissionString;
 }
 
+// Function to check if a file is executable
+bool isExecutable(const std::string& filename) {
+    return access(filename.c_str(), X_OK) == 0;
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " [filename]\n";
@@ -158,6 +164,7 @@ int main(int argc, char** argv) {
     std::cout << "File Size: \033[0;33m" << size << " bytes\033[0m\n";
     std::cout << "Last Modified: \033[1;36m" << std::asctime(std::localtime(&lastModified));
     std::cout << "\033[0mFile Type: \033[1;32m" << fileType << "\033[0m\n";
+    std::cout << "Linux Executable: " << (executable ? "\x1b[1;35mYes\x1b[0m" : "\x1b[1;35mNo\x1b[0m") << "\n";
     std::cout << "File Permissions: \033[0;31m" << permissionString << "\033[0m\n";
 
     file.close();
